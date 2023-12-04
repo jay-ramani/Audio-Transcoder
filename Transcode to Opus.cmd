5f3e1b754f441b335ac838b3478a9f1508beee9d
Jay <7327430+jay-ramani@users.noreply.github.com>
2023-12-04 03:06:56 -0500

    Add files via upload
    DOS shell script to encode to Opus

---
+++ Transcode to Opus.cmd
@@ -0,0 +1,10 @@
+@echo off
+cls
+set PATH=%PATH%;"C:\Program Files\Python"
+:loop_transcode
+IF %1=="" GOTO completed
+python "G:\My Drive\Projects\Transcode and Move Audio Files\transcode_and_move_audio_files.py" --source %1 --encode-to opus --percentage-completion
+SHIFT
+GOTO loop_transcode
+:completed
+pause
